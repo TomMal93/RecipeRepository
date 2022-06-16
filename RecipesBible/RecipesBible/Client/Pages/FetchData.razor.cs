4eb43cc3ba10c686e4447652d50bc0967968d4e8
TomMal93 <Tomasz.Maleszyk93@gmail.com>
2022-06-16 20:29:13 +0200

    Move c# code to codebehind

---
+++ RecipesBible/RecipesBible/Client/Pages/FetchData.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using RecipesBible.Shared;
+using System.Net.Http.Json;
+
+namespace RecipesBible.Client.Pages
+{
+    public partial class FetchData : ComponentBase
+    {
+        private WeatherForecast[]? forecasts;
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast");
+        }
+    }
+}
