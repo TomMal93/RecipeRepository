6fe2f0fd44945f372bebf28a6561d67739da16ff
TomMal93 <Tomasz.Maleszyk93@gmail.com>
2022-07-24 19:53:54 +0200

    Add test service implementation

--- RecipesBible/RecipesBible/Client/Program.cs
+++ RecipesBible/RecipesBible/Client/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RecipesBible.Client;
+using RecipesBible.Client.Service;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ITestService, TestService>();
 
 await builder.Build().RunAsync();
---
+++ RecipesBible/RecipesBible/Client/Service/TestService.cs
@@ -0,0 +1,19 @@
+﻿namespace RecipesBible.Client.Service
+{
+    public interface ITestService
+    {
+        void Test();
+    }
+    public class TestService : ITestService
+    {
+        public TestService()
+        {
+
+        }
+
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
