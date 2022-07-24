3384c675bc8ced4c4961c5092f1c6627bc1c348a
TomMal93 <Tomasz.Maleszyk93@gmail.com>
2022-07-24 19:59:13 +0200

    Move code with TestService to separate file

---
+++ RecipesBible/RecipesBible/Client/Pages/Counter.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using RecipesBible.Client.Service;
+
+namespace RecipesBible.Client.Pages
+{
+    public partial class Counter : ComponentBase
+    {
+        [Inject]
+        private ITestService tService { get; set; }
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+            tService.Test();
+        }
+    }
+}
